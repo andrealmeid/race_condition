1d0832f4cff24bd8b70cb53cc68c6506c3f80ae1
André Almeida <andrealmeid@riseup.net>
2021-04-03 22:08:35 -0300

    state of art videogame

--- file.js
+++ file.js
@@ -3,6 +3,7 @@ let context = canvas.getContext("2d");
 
 var globalX = 0;
 var globalY = 0;
+var globalScale = 1;
 
 canvas.width = 1280;
 canvas.height = 720;
@@ -15,23 +16,25 @@ image.onload = function() {
 
 image.src = "assets/car.png";
 
-function normalize(x, y) {
+function normalize(x, y)
+{
 	let norm = Math.sqrt(x*x + y*y);
 	return [x / norm, y / norm];
 }
 
-function generateTrack() {
+function generateTrack()
+{
 	let track = [canvas.width / 2, canvas.height / 2];
 
 	let dir = [1, 0];
 
-	let scale = 25;
+	let scale = 60;
 
 	for (let i = 2; i < 300; i += 2) {
 		track.push(track[i-2] + dir[0] * scale);
 		track.push(track[i-1] + dir[1] * scale);
 
-		let angle = 0.5;
+		let angle = 0.3;
 		let rand = Math.floor(Math.random() * 3);
 		if (rand == 0)
 			angle = 0;
@@ -50,20 +53,18 @@ function generateTrack() {
 	return track;
 }
 
-var track = generateTrack();
-
-function drawTrack()
+function generateBorders(track)
 {
-	let scale = 20;
+	let trackRadius = 100;
 
 	let posX1 = track[0];
 	let posX2 = track[0];
 	let posY1 = track[1];
 	let posY2 = track[1];
 
-	context.clearRect(0, 0, canvas.width, canvas.height);
+	let borderL = [];
+	let borderR = [];
 
-	context.beginPath();
 	for (let i = 2; i < track.length; i += 2) {
 		let dirX = track[i] - track[i-2];
 		let dirY = track[i+1] - track[i-1];
@@ -73,32 +74,67 @@ function drawTrack()
 		let newDirX2 = -dir[1];
 		let newDirY2 = dir[0];
 
-		context.moveTo(posX1 + globalX, posY1 + globalY);
-		posX1 = track[i] + newDirX1 * scale;
-		posY1 = track[i+1] + newDirY1 * scale;
-		context.lineTo(posX1 + globalX, posY1 + globalY);
+		posX1 = track[i] + newDirX1 * trackRadius;
+		posY1 = track[i+1] + newDirY1 * trackRadius;
 
-		context.moveTo(posX2 + globalX, posY2 + globalY);
-		posX2 = track[i] + newDirX2 * scale;
-		posY2 = track[i+1] + newDirY2 * scale;
-		context.lineTo(posX2 + globalX, posY2 + globalY);
+		borderL.push(posX1);
+		borderL.push(posY1);
+
+		posX2 = track[i] + newDirX2 * trackRadius;
+		posY2 = track[i+1] + newDirY2 * trackRadius;
+
+		borderR.push(posX2);
+		borderR.push(posY2);
+	}
+
+	return [borderL, borderR];
+}
+
+function drawTrack(borders)
+{
+	let [borderL, borderR] = borders;
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	context.lineWidth = 5;
+	context.beginPath();
+	for (let i = 0; i < borderL.length - 2; i += 2) {
+		context.moveTo(borderL[i], borderL[i+1]);
+		context.lineTo(borderL[i+2], borderL[i+3]);
+
+		context.moveTo(borderR[i], borderR[i+1]);
+		context.lineTo(borderR[i+2], borderR[i+3]);
 
 	}
 	context.stroke();
-	window.requestAnimationFrame(drawTrack); 
+	window.requestAnimationFrame(() => drawTrack(borders));
 }
 
-drawTrack(); 
+let track = generateTrack();
+let borders = generateBorders(track);
+drawTrack(borders);
 
 document.onkeypress = function (e) {
-	let speed = 20;
+	let speed = 35;
+	let offsetX = 0, offsetY = 0;
+
 	e = e || window.event;
 	if (e.key === "w")
-		globalY += speed;
+		offsetY = speed;
 	else if (e.key == "s")
-		globalY -= speed;
+		offsetY = -speed;
 	else if (e.key == "a")
-		globalX += speed;
+		offsetX = speed;
 	else if (e.key == "d")
-		globalX -= speed;
+		offsetX = -speed;
+
+	context.translate(offsetX, offsetY);
 };
+
+
+function zoom(event)
+{
+	globalScale -= event.deltaY * 0.01;
+	context.scale(globalScale, globalScale);
+}
+
+canvas.addEventListener("wheel", zoom);

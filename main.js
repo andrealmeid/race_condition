0efcad438a1ff8bb42b64f025d9fa68a9bbe10c0
Guilherme Valarini <guilherme.a.valarini@gmail.com>
2021-04-10 21:07:47 -0300

    Pushando em português

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
-// Configs
 // Movement
 const panSpeedFactor = 1000;
-const zoomSpeedFactor = 1.1;
+const zoomSpeedFactor = 1.2;
 const minZoom = 0.5;
 const maxZoom = 5;
 // Track
 const trackSegmentScale = 50;
-const trackCurveAngles = 10;
+const trackCurveAngles = 15;
 const trackRadius = 50;
 const middleTrackColor = "yellow";
 const middleTrackWidth = 5;
 const borderColor = "black";
 const borderWidth = 10;
+let track = undefined;
+let borders = undefined;
 // Car
 const carScale = 0.05;
+const carAccel = 100;
+const carDeAccel = 1.0 * carAccel;
+const carAngularSpeed = 2;
+const carMaxSpeed = 1000;
+const carMinSpeed = -100;
+let carSpeed = 0;
+let car = undefined;
+
+let stats = {
+  min: Number.MAX_SAFE_INTEGER,
+  max: 0,
+  avg: 0,
+}
 
 // Globals
 const canvas = document.getElementById("mainCanvas");
 
 // Helpers
+function clamp(value, min, max) {
+  return Math.max(min, Math.min(max, value));
+}
+
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -54,8 +72,8 @@ function generateBorders(track) {
     let curveDir = curve.point2 - curve.point1;
     curveDir = curveDir.normalize();
 
-    let leftDir = curveDir.rotate(90);
-    let rightDir = curveDir.rotate(-90);
+    let leftDir = curveDir.rotate(-90);
+    let rightDir = curveDir.rotate(90);
 
     let leftBorderPoint = curve.point1 + leftDir * trackRadius;
     let rightBorderPoint = curve.point1 + rightDir * trackRadius;
@@ -68,8 +86,8 @@ function generateBorders(track) {
   let curveDir = track.lastCurve.point1 - track.lastCurve.point2;
   curveDir = curveDir.normalize();
 
-  let leftDir = curveDir.rotate(-90);
-  let rightDir = curveDir.rotate(90);
+  let leftDir = curveDir.rotate(90);
+  let rightDir = curveDir.rotate(-90);
 
   let leftBorderPoint = track.lastCurve.point2 + leftDir * trackRadius;
   let rightBorderPoint = track.lastCurve.point2 + rightDir * trackRadius;
@@ -106,6 +124,7 @@ function onMouseDrag(event) {
 function onFrame(event) {
   let pan = new Point(0, 0);
   const actualPanSpeed = panSpeedFactor * (1 / view.zoom) * event.delta;
+
   if (Key.isDown("w"))
     pan.y += actualPanSpeed
   if (Key.isDown("s"))
@@ -116,6 +135,36 @@ function onFrame(event) {
     pan.x -= actualPanSpeed
 
   view.translate(pan);
+
+  // Car control
+  if (car !== undefined) {
+    let carRotation = 0;
+
+    if (Key.isDown("up"))
+      carSpeed += carAccel * event.delta;
+    else if (carSpeed > 0)
+      carSpeed -= carDeAccel * event.delta;
+    if (Key.isDown("down"))
+      carSpeed -= carAccel * event.delta;
+    else if (carSpeed < 0)
+      carSpeed += carDeAccel * event.delta;
+    if (Key.isDown("left"))
+      carRotation -= carAngularSpeed;
+    if (Key.isDown("right"))
+      carRotation += carAngularSpeed;
+    carRotation *= carSpeed > 0 ? carSpeed / carMaxSpeed : -carSpeed / carMinSpeed;
+
+    carSpeed = Math.round(clamp(carSpeed, carMinSpeed, carMaxSpeed));
+
+    car.rotate(carRotation);
+    const carDir = new Point(1, 0).rotate(car.rotation);
+    car.translate(carDir * carSpeed * event.delta);
+  }
+
+  stats.min = Math.min(stats.min, event.delta);
+  stats.max = Math.max(stats.max, event.delta);
+  stats.avg = stats.avg * 0.3 + event.delta * 0.7;
+  console.log(stats);
 }
 
 canvas.addEventListener("wheel", event => {
@@ -129,6 +178,6 @@ canvas.addEventListener("wheel", event => {
 });
 
 // Main code
-let track = generateTrack();
-let borders = generateBorders(track);
-let car = loadCar();
+track = generateTrack();
+borders = generateBorders(track);
+car = loadCar();

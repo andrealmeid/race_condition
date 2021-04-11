3e89172119a35e0156e81272d698fc9e37656792
Italo Nicola <italo.niccola@github.com>
2021-04-11 02:44:06 -0300

    Change backend port

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(80); //the server object listens on port 8080 
+}).listen(8080); //the server object listens on port 8080 

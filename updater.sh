7df3cdc43bc2855227b0c850e1969a2237053f1e
Italo Nicola <italo.niccola@github.com>
2021-04-11 02:31:46 -0300

    Add minimal server and self-updater

---
+++ updater.sh
@@ -0,0 +1,13 @@
+while :
+do
+	git fetch origin
+	if [[ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]]; then
+		git reset FETCH_HEAD --hard
+
+		killall nodejs
+		nodejs server.js
+	fi
+
+	sleep 15
+done
+

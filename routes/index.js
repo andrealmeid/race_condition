b624bea59c9a8cfe59ae12d9e06ebf808c63429c
Tales L. da Aparecida <tales.aparecida@gmail.com>
2021-04-12 00:07:32 -0300

    Show commit hash in the title

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const childProcess = require("child_process");
+
+const gitCommitHash = childProcess.execSync("git rev-parse --short HEAD");
+
+const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-	res.render("index", { title: "Race Condition" });
+	res.render("index", { title: `Race Condition (${gitCommitHash})` });
 });
 
 module.exports = router;

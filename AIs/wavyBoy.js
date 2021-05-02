a21e55063ddb44e8d64744fe7bcec071eeb5b801
Lucas Cunha <lcagustini@users.noreply.github.com>
2021-05-02 02:46:31 -0300

    New and improved wavyBoy

--- AIs/wavyBoy.js
+++ AIs/wavyBoy.js
@@ -1,9 +1,10 @@
 let wavyBoy = function (sensors, speed, isOffroad, input) {
-  input.down = false;
+  input.up = true;
   input.left = false;
   input.right = false;
-  input.up = true;
 
-  if (sensors[9] < 100) input.right = true;
-  else if (sensors[10] > sensors[9]) input.left = true;
+  let diff = sensors[10] - sensors[2];
+
+  if (diff > 50) input.left = true;
+  else if (diff < -50) input.right = true;
 }
